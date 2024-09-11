41c16c0cfa77dce904d9223202f2630ab5654d04
Sangwoo <jswoo1995@gmail.com>
2024-09-12 00:03:10 +0900

    주석 처리

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -6,16 +6,16 @@ import { fetchSearchVideo } from "../utils/api";
 
 const Search = () => {
     const { searchId } = useParams();
-    const [videos, setVideos] = useState([]);
-    const [nextPageToken, setNextPageToken] = useState(null);
+    // const [videos, setVideos] = useState([]);
+    // const [nextPageToken, setNextPageToken] = useState(null);
 
     useEffect(() => {
         fetchSearchVideo(searchId).then((data) => setVideos(data.items));
     }, [searchId]);
 
-    const fetchMoreVideo = (query, pageToken) => {
-        fetchSearchVideo();
-    };
+    // const fetchMoreVideo = (query, pageToken) => {
+    //     fetchSearchVideo();
+    // };
     return (
         <Main title="유투브 검색" description="유튜브 검색 결과 페이지입니다.">
             <section id="searchPage">
