5499d3ff6667e1e6875d7f55b9a02e83ca7d3a52
Sangwoo <jswoo1995@gmail.com>
2024-09-12 00:39:53 +0900

    [fix] axios 경로 수정

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,17 +1,15 @@
 import axios from "axios";
 
 export const BASE_URL = axios.create({
-    baseURL:"https://www.googleapis.com/youtube/v3",
-})
+    baseURL: "https://www.googleapis.com/youtube/v3",
+});
 
 export const fetchSearchVideo = async (searchId, url) => {
     const api_key = process.env.REACT_APP_YOUTUBE_API_KEY;
-    const { data } = await axios.get(
-        `${BASE_URL}/search?part=snippet&maxResults=48&q=${searchId}&type=video&key=${api_key}`
+    const { data } = await BASE_URL.get(
+        `/search?part=snippet&maxResults=48&q=${searchId}&type=video&key=${api_key}`
     );
 
     console.log(data);
     return data;
 };
-
-// https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=48&q=${searchId}&type=video&key=${process.env.REACT_APP_YOUTUBE_API_KEY}
