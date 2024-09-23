30dcbd8672f0a791ce2af1bac04d72784d1dcef5
Sangwoo <jswoo1995@gmail.com>
2024-09-24 00:14:46 +0900

    test12

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -7,14 +7,15 @@ import { fetchSearchVideo } from "../utils/api";
 const Search = () => {
     const { searchId } = useParams();
     const [videos, setVideos] = useState([]);
-    // const [nextPageToken, setNextPageToken] = useState(null);
+    const [isLoading, setIsLoading] = useState(true);
 
     useEffect(() => {
-        fetchSearchVideo(searchId)
-            .then((result) => setVideos(result.items || []))
-            .catch((error) => {
-                console.error("error fetching search results:", error);
-            });
+        setIsLoading(true);
+        fetchSearchVideo(searchId).then((result) => setVideos(result.items || []));
+        setIsLoading(false).catch((error) => {
+            console.error("error fetching search results:", error);
+            setIsLoading(false);
+        });
     }, [searchId]);
 
     console.log(videos);
@@ -26,9 +27,13 @@ const Search = () => {
         <Main title="유투브 검색" description="유튜브 검색 결과 페이지입니다.">
             <section id="searchPage">
                 <div className="video__inner search">
-                    <Suspense fallback={<div>Loading...........</div>}>
-                        <VideoSearch videos={videos} />
-                    </Suspense>
+                    {isLoading ? (
+                        <div>Loading... </div>
+                    ) : (
+                        <Suspense fallback={<div>Loading...........</div>}>
+                            <VideoSearch videos={videos} />
+                        </Suspense>
+                    )}
                 </div>
             </section>
         </Main>
