6bfb73c42402fd10880b4e091472b64ece08053d
Sangwoo <jswoo1995@gmail.com>
2024-09-24 00:29:41 +0900

    test17

--- src/components/videos/VIdeoSearch.jsx
+++ src/components/videos/VIdeoSearch.jsx
@@ -2,28 +2,25 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const VideoSearch = ({ videos }) => {
+    console.log(videos);
     return (
         <>
             {videos.map((video, index) => (
                 <div className="video" key={index}>
                     <div className="video__thumb play__icon">
                         <Link
-                            to={`/video/${video.items.id.videoId}`}
-                            style={{
-                                backgroundImage: `url(${video.items.snippet.thumbnails.high.url})`,
-                            }}
+                            to={`/video/${video.id.videoId}`}
+                            style={{ backgroundImage: `url(${video.snippet.thumbnails.high.url})` }}
                         ></Link>
                     </div>
                     <div className="video__info">
                         <div className="title">
-                            <Link to={`/video.items/${video.items.id.videoId}`}>
-                                {video.items.snippet.title}
-                            </Link>
+                            <Link to={`/video/${video.id.videoId}`}>{video.snippet.title}</Link>
                         </div>
                         <div className="info">
                             <span className="author">
-                                <Link to={`/channel/${video.items.snippet.channelId}`}>
-                                    {video.items.snippet.channelTitle}
+                                <Link to={`/channel/${video.snippet.channelId}`}>
+                                    {video.snippet.channelTitle}
                                 </Link>
                             </span>
                         </div>
