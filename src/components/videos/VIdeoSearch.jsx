f72dd74e53c4def0d2afd59191c79886e0d46572
Sangwoo <jswoo1995@gmail.com>
2024-09-24 00:40:01 +0900

    test 19

--- src/components/videos/VIdeoSearch.jsx
+++ src/components/videos/VIdeoSearch.jsx
@@ -5,32 +5,36 @@ const VideoSearch = ({ videos }) => {
     console.log(videos);
     return (
         <>
-            {videos.map((video, index) => (
-                <div className="video" key={index}>
-                    <div className="video__thumb play__icon">
-                        <Link
-                            to={`/video/${video.items.id.videoId}`}
-                            style={{
-                                backgroundImage: `url(${video.items.snippet.thumbnails.high.url})`,
-                            }}
-                        ></Link>
-                    </div>
-                    <div className="video__info">
-                        <div className="title">
-                            <Link to={`/video.items/${video.items.id.videoId}`}>
-                                {video.items.snippet.title}
-                            </Link>
+            {Array.isArray(videos.items) && videos.items.length > 0 ? (
+                videos.map((video, index) => (
+                    <div className="video" key={index}>
+                        <div className="video__thumb play__icon">
+                            <Link
+                                to={`/video/${video.items.id.videoId}`}
+                                style={{
+                                    backgroundImage: `url(${video.items.snippet.thumbnails.high.url})`,
+                                }}
+                            ></Link>
                         </div>
-                        <div className="info">
-                            <span className="author">
-                                <Link to={`/channel/${video.items.snippet.channelId}`}>
-                                    {video.items.snippet.channelTitle}
+                        <div className="video__info">
+                            <div className="title">
+                                <Link to={`/video.items/${video.items.id.videoId}`}>
+                                    {video.items.snippet.title}
                                 </Link>
-                            </span>
+                            </div>
+                            <div className="info">
+                                <span className="author">
+                                    <Link to={`/channel/${video.items.snippet.channelId}`}>
+                                        {video.items.snippet.channelTitle}
+                                    </Link>
+                                </span>
+                            </div>
                         </div>
                     </div>
-                </div>
-            ))}
+                ))
+            ) : (
+                <div>video없어 임마</div>
+            )}
         </>
     );
 };
