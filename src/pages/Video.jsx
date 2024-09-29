4de010515d53d10a324335b1301af74959697b35
Sangwoo <jswoo1995@gmail.com>
2024-09-29 15:35:32 +0900

    test video detail

--- src/pages/Video.jsx
+++ src/pages/Video.jsx
@@ -1,10 +1,20 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Main from "../components/section/Main";
+import { useParams } from "react-router-dom";
+import { fetchYoutubeAPI } from "../utils/api";
 
 const Video = () => {
+    const { videoId } = useParams();
+    const [videoDetail, setVideoDetail] = useState(null);
+
+    useEffect(() => {
+        fetchYoutubeAPI(`videos?part=snippet,statistics&id=${videoId}`).then((data) => {
+            console.log(data);
+        });
+    }, [videoId]);
     return (
         <Main title="유튜브 비디오 영상" description="유튜브 비디오 영상을 볼 수 있습니다.">
-            Video
+            <section></section>
         </Main>
     );
 };
--- src/utils/api.js
+++ src/utils/api.js
@@ -1,27 +1,17 @@
 import axios from "axios";
 
-// export const BASE_URL = axios.create({
-//     baseURL: "https://www.googleapis.com/youtube/v3",
-// });
-
-// export const fetchSearchVideo = async (searchId, url) => {
-//     const api_key = process.env.REACT_APP_YOUTUBE_API_KEY;
-//     const { data } = await BASE_URL.get(
-//         `/search?part=snippet&maxResults=48&q=${searchId}&type=video&key=${api_key}`
-//     );
-
-//     console.log(data);
-//     return data;
-// };
+export const BASE_URL = "/.netlify/functions/fetchYoutubeData";
+export const fetchYoutubeAPI = async (url) => {
+    const { data } = await axios.get(`${BASE_URL}/${url}`);
+    console.log(data);
+    return data;
+};
 
 export const fetchSearchVideo = async (searchId) => {
     try {
-        const res = await axios.get(
-            `/.netlify/functions/fetchYoutubeData?q=${encodeURIComponent(searchId)}`,
-            {
-                withCredentials: true,
-            }
-        );
+        const res = await axios.get(`${BASE_URL}?q=${encodeURIComponent(searchId)}`, {
+            withCredentials: true,
+        });
 
         console.log("server : ", res.data.items);
         return res;
