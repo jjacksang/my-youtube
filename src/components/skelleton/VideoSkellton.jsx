cce40436a27856209690d014238e7979d69197c8
Sangwoo <jswoo1995@gmail.com>
2024-10-20 17:51:17 +0900

    test: VideoSkellton test

---
+++ src/components/skelleton/VideoSkellton.jsx
@@ -0,0 +1,17 @@
+export default function VideoSkellton() {
+    return (
+        <>
+            <div className="video">
+                <div className="video__thumb play__icon">
+                    <div className="video__skellton"></div>
+                </div>
+                <div className="video__info">
+                    <div className="title"></div>
+                    <div className="info">
+                        <span className="author"></span>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+}
