f005628c447d2d8c69cc0fe874c7653bbcc9acca
Sangwoo <jswoo1995@gmail.com>
2024-10-25 17:33:31 +0900

    test

--- netlify/functions/fetchYoutubeData.js
+++ netlify/functions/fetchYoutubeData.js
@@ -39,7 +39,7 @@ exports.handler = async (event, context) => {
                 },
                 params: {
                     part: "snippet",
-                    maxResults: 20,
+                    maxResults: (maxResults += 20),
                     q: q,
                     type: "video",
                     nextPageToken: nextPageToken || undefined,
@@ -81,7 +81,7 @@ exports.handler = async (event, context) => {
                     part: "snippet",
                     channelId: channelId,
                     type: "playlist",
-                    maxResults: 20,
+                    maxResults: (maxResults += 20),
                     order: "date",
                     nextPageToken: nextPageToken || undefined,
                     key: api_key,
