e4a19e175df69e54aa38964335ff7dea67c7f1f0
Sangwoo <jswoo1995@gmail.com>
2024-11-04 17:21:09 +0900

    fix: Logo > Link to

--- src/components/header/Logo.jsx
+++ src/components/header/Logo.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 const Logo = ({ toggleMenu }) => {
     return (
         <h1 className="header__logo">
-            <Link href="/">
+            <Link to="/">
                 <em aria-hidden="true" onClick={toggleMenu}></em>
                 <span>
                     chop
