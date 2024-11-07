8eb9f956ae5850c18481c654594e91aced13c9b0
Sangwoo <jswoo1995@gmail.com>
2024-11-07 21:28:36 +0900

    test: cache test

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,21 +1,38 @@
 import axios from "axios";
 
 export const BASE_URL = "/.netlify/functions/fetchYoutubeData";
-export const fetchYoutubeAPI = async (videoId) => {
-    const res = await axios.get(`${BASE_URL}?id=${videoId}`);
-    console.log(res);
-    return res;
+
+// 캐시데이터 저장
+const cache = {
+    searchCache: {},
+    channelCache: {},
 };
 
-export const fetchSearchVideo = async (searchId, nextPageToken, maxResults = 24) => {
+export const fetchSearchVideo = async (
+    searchId,
+    nextPageToken,
+    maxResults = 24
+) => {
+    // 캐시 고유 키 생성
+    const cacheKey = `search-${searchId}-${nextPageToken || ""}`;
+
+    // 캐시값이 있으면 그대로 캐시값을 돌려줌
+    if (cache.searchCache[cacheKey]) {
+        console.log("Fetcing from cache: ", cacheKey);
+        return cache.searchCache[cacheKey];
+    }
+
     try {
-        const res = await axios.get(`${BASE_URL}?q=${encodeURIComponent(searchId)}`, {
-            params: {
-                q: searchId,
-                pageToken: nextPageToken || "",
-                maxResults: maxResults,
-            },
-        });
+        const res = await axios.get(
+            `${BASE_URL}?q=${encodeURIComponent(searchId)}`,
+            {
+                params: {
+                    q: searchId,
+                    pageToken: nextPageToken || "",
+                    maxResults: maxResults,
+                },
+            }
+        );
 
         console.log("nextPageToken: ", nextPageToken);
         console.log("server : ", res.data.items);
@@ -25,6 +42,18 @@ export const fetchSearchVideo = async (searchId, nextPageToken, maxResults = 24)
             console.error("Unexpected response structure:", res.data);
             throw new Error("Unexpected response structure");
         }
+
+        // 캐시에 데이터 저장
+        cache.searchCache[cacheKey] = res.data;
+
+        // 일정 시간 이후 캐시 삭제
+        setTimeout(() => {
+            delete cache.searchCache[cacheKey];
+            console.log(
+                `Cache for ${cacheKey} has been removed after timeout.`
+            );
+        }, 60000);
+
         return res.data;
     } catch (error) {
         console.error("Client : Error fetching data => ", error);
@@ -58,7 +87,18 @@ export const fetchChannel = async (channelId) => {
     }
 };
 
-export const fetchChannelVideo = async (channelId, searchType, nextPageToken, maxResults = 24) => {
+export const fetchChannelVideo = async (
+    channelId,
+    searchType,
+    nextPageToken,
+    maxResults = 24
+) => {
+    const cacheKey = `channel-${channelId}-${nextPageToken || ""}`;
+
+    if (cache.channelCache[cacheKey]) {
+        return cache.channelCache[cacheKey];
+    }
+
     try {
         const res = await axios.get(`${BASE_URL}`, {
             params: {
